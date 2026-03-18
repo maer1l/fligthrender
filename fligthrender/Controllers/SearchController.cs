76397e846c57d9907c94a36ed0f6b6c48ca871c0
burykinburyk2004-lang <burykin.buryk2004@gmail.com>
2026-03-18 09:17:22 +0300

    Add Search no ajax

---
+++ fligthrender/Controllers/SearchController.cs
@@ -0,0 +1,48 @@
+﻿using fligthrender.Data;
+using fligthrender.Models;
+using fligthrender.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.ConstrainedExecution;
+
+namespace fligthrender.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly FlightrenderContext _context;
+
+        public SearchController(FlightrenderContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                return View(new SearchPlanesManufacturer());
+            }
+
+            SearchPlanesManufacturer result = new SearchPlanesManufacturer();
+
+            var planes = _context.Planes
+             .Where(c => c.Model.Contains(query)
+             || c.Brand.Name.Contains(query)
+             || c.Year.ToString().Contains(query))
+             .ToList();
+
+            
+            result.Planes.AddRange(planes);
+
+            var manufacturer = _context.Manufacturers
+            .Where(m => m.Name.Contains(query)
+            || m.Description.Contains(query)
+            || m.Address.Contains(query))
+            .ToList();
+
+
+            result.Manufacturers.AddRange(manufacturer);
+
+            return View(result);
+        }
+    }
+}
---
+++ fligthrender/ViewModels/SearchPlanesManufacturer.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+using fligthrender.Models;
+using Plane = fligthrender.Models.Plane;
+
+namespace fligthrender.ViewModels
+{
+    public class SearchPlanesManufacturer
+    {
+        List<Plane> planes = new List<Plane>();
+
+        List<Manufacturer> manufacturers = new List<Manufacturer>();
+
+        public List<Plane> Planes { get => planes; set => planes = value; }
+        public List<Manufacturer> Manufacturers { get => manufacturers; set => manufacturers = value; }
+
+        public Plane PlaneInfo { get => planes[0]; set => planes[0] = value; }
+        public Manufacturer ManufacturerInfo { get => manufacturers[0]; set => manufacturers[0] = value; }
+    }
+}
