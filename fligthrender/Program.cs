30162d7c4df67a1350e347be44597a0921b6f2be
burykinburyk2004-lang <burykin.buryk2004@gmail.com>
2026-04-08 09:44:58 +0300

    Test controller

--- fligthrender/Program.cs
+++ fligthrender/Program.cs
@@ -93,7 +93,7 @@ namespace fligthrender
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=Main}/{action=Index}/{id?}")
                 .WithStaticAssets();
 
             app.Run();
