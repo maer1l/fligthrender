5eb130ad63d04895ad1744c870728edbe9aaa8b4
burykinburyk2004-lang <burykin.buryk2004@gmail.com>
2026-04-14 15:42:45 +0300

    add show flight

---
+++ fligthrender/Controllers/FlightsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace fligthrender.Controllers
+{
+    public class FlightsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
